6e94161079b980ea0471cfa8052aad5bb30fa150
ngactive99 <ngactive99@gmail.com>
2022-05-01 22:32:52 +0530

    created sagas

---
+++ src/sagas/index.js
@@ -0,0 +1,16 @@
+import {put,takeLatest,all} from 'redux-saga/effects'
+
+function* fetchNews() {
+    const json = yield fetch("https://ngapi4.herokuapp.com/api/getProducts")
+    .then(response => response.json());
+    yield put({then: "NEWS_RECEIVED", json: json})
+}
+function* actionWatcher() {
+    yield takeLatest('GET_NEWS', fetchNews)
+}
+
+export default function* rootSaga() {
+    yield all([
+        actionWatcher(),
+    ])
+}
